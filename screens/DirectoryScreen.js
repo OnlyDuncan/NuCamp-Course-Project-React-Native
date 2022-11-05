dbcef20b126e7a579b8559cf01bb4cab9769d583
Duncan Payne <Duncan.C.Payne@gmail.com>
2022-11-04 21:11:57 -0500

    Everything

---
+++ screens/DirectoryScreen.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+
+const DirectoryScreen = ({ navigation }) => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+
+    const renderDirectoryItem = ({ item: campsite }) => {
+        return (
+            <ListItem
+                onPress={() =>
+                    navigation.navigate('CampsiteInfo', { campsite })
+                }
+            >
+                <Avatar source={campsite.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{campsite.name}</ListItem.Title>
+                    <ListItem.Subtitle>
+                        {campsite.description}
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
+    return (
+        <FlatList
+            data={campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={(item) => item.id.toString()}
+        />
+    );
+};
+
+export default DirectoryScreen;
